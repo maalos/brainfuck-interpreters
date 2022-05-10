5b61a25d56da07d073a1ca735bc5a06859c17596
maalos <54448857+maalos@users.noreply.github.com>
2022-05-10 23:17:04 +0200

    IT'S ABSOLUETY TERRIBLE, BUT IT WORKS!
    ben

---
+++ brainfuck-to-bash.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+c=`cat $1`
+f="bbi.tmp"
+echo $c
+
+for (( i=0; i<${#c}; i++ )); do
+    case ${c:$i:1} in
+        "+") echo "(( a[p] = a[p] + 1 ))" >> $f ;;
+        "-") echo "(( a[p] = a[p] - 1 ))" >> $f;;
+        ">") echo "(( p = p + 1 ))" >> $f ;;
+        "<") echo "(( p = p - 1 ))" >> $f ;;
+        ".") echo 'printf "\x$(printf %x ${a[$p]})"' >> $f ;;
+        "[") echo "while [ \${a[\$p]} -ne 0 ]; do" >> $f ;;
+        "]") echo "done" >> $f ;;
+        *) echo ${c:$i:1} ;;
+    esac
+done
+
+bash $f
+rm $f
